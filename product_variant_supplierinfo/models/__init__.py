61b2efb78c8bbe1c1465ab237bb09728e681d564
Alex Comba <alex.comba@agilebg.com>
2016-01-21 17:06:51 +0100

    Modify files accoding to OCA template

--- product_variant_supplierinfo/models/__init__.py
+++ product_variant_supplierinfo/models/__init__.py
@@ -1,6 +1,6 @@
-# -*- encoding: utf-8 -*-
-##############################################################################
-# For copyright and license notices, see __openerp__.py file in root directory
-##############################################################################
+# -*- coding: utf-8 -*-
+# © 2015 Serv. Tecnol. Avanzados - Pedro M. Baeza
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
 from . import product_supplierinfo
 from . import product_product
