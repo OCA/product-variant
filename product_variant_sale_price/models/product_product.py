c4169a77893a790258a417a8e9057f04742a4856
Sergio Incaser <sergio@incaser.es>
2016-04-25 20:14:57 +0200

    [NEW][8.0] product_variant_sale_price: lst_price in variant

--- product_variant_sale_price/models/product_product.py
+++ product_variant_sale_price/models/product_product.py
@@ -5,38 +5,27 @@
 from openerp import models, fields, api
 
 
-class ProductTemplate(models.Model):
-    _inherit = "product.template"
-
-    def _compute_is_variant(self):
-        print self.env.context
-        if self.env.context.get('active_model') != 'product.template':
-            self.is_variant = True
-
-    is_variant = fields.Boolean(compute='_compute_is_variant')
-
-
 class ProductProduct(models.Model):
     _inherit = "product.product"
 
     @api.multi
+    @api.depends('lst_price', 'product_tmpl_id.list_price')
     def _product_lst_price(self):
+        # TODO: review lst_price. Always false
         for product in self:
+            price = product.lst_price or product.list_price
             if 'uom' in self.env.context:
                 uom = product.uos_id or product.uom_id
-                price = uom.with_context(uom='uom')._compute_price(
-                    product.lst_price)
-            else:
-                price = product.lst_price
+                price = uom.with_context(uom='uom')._compute_price(price)
             product.lst_price = price
 
     @api.multi
     def _set_product_lst_price(self):
         if 'uom' in self.env.context:
-            uom = self.uos_id or self.uom_id
-            self.lst_price = uom.with_context(uom='uom')._compute_price(
-                self.lst_price)
-        # return {'lst_price': self.lst_price}
+            for product in self:
+                uom = product.uos_id or product.uom_id
+                product.lst_price = uom.with_context(uom='uom')._compute_price(
+                    product.lst_price)
 
     lst_price = fields.Float(
         compute='_product_lst_price',
