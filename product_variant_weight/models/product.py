f5e1db83498c1cb73b573f8851baea7a70f4c486
sergio-incaser <sergio@incaser.es>
2016-07-02 03:06:09 +0200

    [NEW][8.0] product_variant_weight: Changes suggested by @yajo.

--- product_variant_weight/models/product.py
+++ product_variant_weight/models/product.py
@@ -9,30 +9,27 @@ from openerp.addons import decimal_precision as dp
 class ProductTemplate(models.Model):
     _inherit = 'product.template'
 
-    def _prepare_weight_values(self, vals):
+    def _update_weight_values(self, vals):
         values = {}
         if 'weight' in vals:
             values['weight'] = vals['weight']
         if 'weight_net' in vals:
             values['weight_net'] = vals['weight_net']
-        return values
+        if values:
+            self.product_variant_ids.write(values)
 
     @api.multi
     def write(self, vals):
         res = super(ProductTemplate, self).write(vals)
         for product in self:
-            values = self._prepare_weight_values(vals)
-            if values:
-                product.product_variant_ids.write(values)
+            product._update_weight_values(vals)
         return res
 
     @api.model
     def create(self, vals):
         product_tmpl = super(ProductTemplate, self).create(vals)
         for product in product_tmpl:
-            values = self._prepare_weight_values(vals)
-            if values:
-                product.product_variant_ids.write(values)
+            product._update_weight_values(vals)
         return product_tmpl
 
 
@@ -43,14 +40,17 @@ class ProductProduct(models.Model):
         string='Gross Weight', digits=dp.get_precision('Stock Weight'),
         help='The gross weight in Kg.')
     weight_net = fields.Float(
-        string='Weight Net', digits=dp.get_precision('Stock Weight'),
+        string='Net Weight', digits=dp.get_precision('Stock Weight'),
         help='The net weight in Kg.')
 
     @api.model
     def create(self, vals):
         product = super(ProductProduct, self).create(vals)
-        product.write({
-            'weight': product.product_tmpl_id.weight,
-            'weight_net': product.product_tmpl_id.weight_net,
-        })
+        template_vals = {}
+        if 'weight' not in vals:
+            template_vals['weight'] = product.product_tmpl_id.weight
+        if 'weight_net' not in vals:
+            template_vals['weight_net'] = product.product_tmpl_id.weight_net
+        if template_vals:
+            product.write(template_vals)
         return product
