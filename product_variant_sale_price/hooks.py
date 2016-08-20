1eda2e482f07abefefbaf9e103eebf95225819ca
sergio.teruel <sergio.teruel@tecnativa.com>
2016-08-20 20:29:11 +0200

    [NEW][8.0] product_variant_sale_price: Add test, fixes and translation

--- product_variant_sale_price/hooks.py
+++ product_variant_sale_price/hooks.py
@@ -5,9 +5,22 @@
 
 def set_sale_price_on_variant(cr, registry):
     sql = """
-            UPDATE product_product pp
-            SET fix_price = (SELECT list_price
-                                FROM product_template pt
-                                WHERE pp.product_tmpl_id = pt.id)
+        UPDATE product_product pp
+        SET fix_price = (
+            SELECT (pt.list_price + (SELECT COALESCE( SUM(pap.price_extra), 0)
+                    FROM product_attribute_value_product_product_rel pav_pp_rel
+                    LEFT JOIN product_attribute_price pap ON
+                        pap.value_id = pav_pp_rel.att_id
+                    LEFT JOIN product_product pp_rel ON
+                        pav_pp_rel.prod_id = pp_rel.id
+                    LEFT JOIN product_template pt ON
+                        pp_rel.product_tmpl_id = pt.id
+                    WHERE pav_pp_rel.prod_id = pp.id AND
+                        pap.product_tmpl_id = pt.id
+                )) AS sum_price_extra
+            FROM product_product pp_in LEFT JOIN product_template pt ON
+                pp_in.product_tmpl_id=pt.id
+            WHERE pp_in.id = pp.id
+        );
     """
     cr.execute(sql)
