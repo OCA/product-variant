5234e2cf64d03c5bbe48ceb404a58cfd8270cc93
Guewen Baconnier <guewen.baconnier@camptocamp.com>
2016-09-02 10:23:03 +0200

    Migrate to 9.0

--- product_variant_supplierinfo/__openerp__.py
+++ product_variant_supplierinfo/__openerp__.py
@@ -5,7 +5,7 @@
 {
     'name': 'Product supplier info per variant',
     'summary': 'Supplier info to product variant scope',
-    'version': '8.0.1.0.0',
+    'version': '9.0.1.0.0',
     'author': 'Tecnativa, Akretion,'
               'Odoo Community Association (OCA)',
     'category': 'Product Management',
