a647f69b9b5664789d7fca3488171920f1a57816
Zakaria Makrelouf <z.makrelouf@gmail.com>
2016-10-24 17:29:56 +0200

    [ADD] purchase_variant_configurator: This module has been ported from the odoomrp repo. the following improvements were done while porting this module:
    * Adapt files as per OCA guidelines.
    * Add unit test

---
+++ purchase_product_variant_configurator/models/purchase_order_line.py
@@ -0,0 +1,70 @@
+# -*- coding: utf-8 -*-
+# © 2016 Oihane Crucelaegui - AvanzOSC
+# © 2016 Pedro M. Baeza <pedro.baeza@tecnativa.com>
+# © 2016 ACSONE SA/NV
+# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
+
+from openerp import api, fields, models
+from openerp.tools import DEFAULT_SERVER_DATETIME_FORMAT
+from datetime import datetime
+
+
+class PurchaseOrderLine(models.Model):
+    _inherit = ['purchase.order.line', 'product.configurator']
+    _name = 'purchase.order.line'
+
+    product_id = fields.Many2one(required=False)
+
+    @api.multi
+    def action_duplicate(self):
+        self.ensure_one()
+        self.copy()
+
+    @api.onchange('product_id')
+    def onchange_product_id(self):
+        if self.product_id:
+            res = super(PurchaseOrderLine, self).onchange_product_id()
+            self.onchange_product_id_product_configurator()
+            product_lang = self.product_id.with_context({
+                'lang': self.partner_id.lang,
+                'partner_id': self.partner_id.id,
+            })
+            if product_lang.description_purchase:
+                self.name += '\n' + self.product_id.description_purchase
+            return res
+        return {}
+
+    @api.onchange('product_tmpl_id')
+    def onchange_product_tmpl_id(self):
+        if not self.product_tmpl_id:
+            return {}
+
+        result = super(PurchaseOrderLine, self).onchange_product_tmpl_id()
+
+        self.date_planned = datetime.today().strftime(
+            DEFAULT_SERVER_DATETIME_FORMAT)
+        self.price_unit = self.product_qty = 0.0
+        self.product_uom = self.product_tmpl_id.uom_po_id\
+            or self.product_tmpl_id.uom_id
+        result['domain'].update({
+            'product_uom':
+            [('category_id', '=', self.product_tmpl_id.uom_id.category_id.id)]
+        })
+
+        product_lang = self.product_tmpl_id.with_context({
+            'lang': self.partner_id.lang,
+            'partner_id': self.partner_id.id,
+        })
+        if product_lang.description_purchase:
+            self.name += '\n' + self.product_tmpl_id.description_purchase
+
+        fpos = self.order_id.fiscal_position_id
+        company_id = self.env.user.company_id.id
+        self.taxes_id = fpos.map_tax(
+            self.product_id.supplier_taxes_id.filtered(
+                lambda r: r.company_id.id == company_id)
+        )
+        self._suggest_quantity()
+        self._onchange_quantity()
+
+        return result
--- purchase_product_variants/models/__init__.py
+++
@@ -1,7 +0,0 @@
-# -*- coding: utf-8 -*-
-# © 2015 Oihane Crucelaegui - AvanzOSC
-# © 2015-2016 Serv. Tecnol. Avanzados - Pedro M. Baeza
-# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
-
-from . import purchase_order
-from . import procurement_order
