989bf103bc162bfae5d3e50ab89bdda0e9c4311f
Zakaria Makrelouf <z.makrelouf@gmail.com>
2016-10-25 13:59:30 +0100

    Rename purchase_product_variants > purchase_product_variant_configurator

---
+++ purchase_product_variant_configurator/__openerp__.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
+
+{
+    "name": "Purchase - Product variants",
+    "summary": "Product variants in purchase management",
+    "version": "8.0.2.1.0",
+    "license": "AGPL-3",
+    "depends": [
+        "product",
+        "purchase",
+        "product_variant_configurator",
+    ],
+    "author": "OdooMRP team, "
+              "AvanzOSC, "
+              "Tecnativa, "
+              "Serv. Tecnol. Avanzados - Pedro M. Baeza",
+              "ACSONE SA/NV"
+    "contributors": [
+        "Oihane Crucelaegui <oihanecrucelaegi@avanzosc.es>",
+        "David Díaz <d.diazp@gmail.com>",
+        "Pedro M. Baeza <pedro.baeza@serviciosbaeza.com>",
+        "Ana Juaristi <ajuaristio@gmail.com>",
+        "Zakaria Makrelouf(Acsone) <z.makrelouf@gmail.com>",
+    ],
+    "category": "Purchase Management",
+    "website": "http://www.odoomrp.com",
+    "data": [
+        "views/inherited_purchase_order_views.xml",
+        "views/inherited_purchase_order_line_views.xml",
+    ],
+    "installable": True,
+    "post_init_hook": "assign_product_template",
+}
