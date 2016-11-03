5af27483d504245773dc8bdeb248c20bb340c255
Zakaria Makrelouf <z.makrelouf@gmail.com>
2016-11-03 08:53:25 +0100

    [REF] Revert onchange_product_attribute_ids and make it behave exactly like onchange_product_id

--- purchase_product_variant_configurator/models/purchase_order_line.py
+++ purchase_product_variant_configurator/models/purchase_order_line.py
@@ -6,6 +6,7 @@
 
 from openerp import api, fields, models
 from openerp.tools import DEFAULT_SERVER_DATETIME_FORMAT
+
 from datetime import datetime
 
 
@@ -14,20 +15,6 @@ class PurchaseOrderLine(models.Model):
     _name = 'purchase.order.line'
 
     product_id = fields.Many2one(required=False)
-
-    @api.onchange('product_attribute_ids')
-    def onchange_product_attribute_ids(self):
-        result = super(PurchaseOrderLine, self).\
-            onchange_product_attribute_ids()
-        if self.product_attribute_ids and not self.product_id:
-            product_obj = self.env['product.product']
-            values = self.product_attribute_ids.mapped('value_id.id')
-            if len(values) == len(self.product_attribute_ids):
-                self.product_id = product_obj.create({
-                    'product_tmpl_id': self.product_tmpl_id.id,
-                    'attribute_value_ids': [(4, value) for value in values]
-                })
-        return result
 
     @api.onchange('product_tmpl_id')
     def onchange_product_tmpl_id(self):
@@ -39,11 +26,12 @@ class PurchaseOrderLine(models.Model):
         self.date_planned = datetime.today().strftime(
             DEFAULT_SERVER_DATETIME_FORMAT)
         self.price_unit = self.product_qty = 0.0
-        self.product_uom = self.product_tmpl_id.uom_po_id\
-            or self.product_tmpl_id.uom_id
+        self.product_uom = self.product_tmpl_id.uom_po_id or\
+            self.product_tmpl_id.uom_id
         result['domain'] = {
             'product_uom':
-            [('category_id', '=', self.product_tmpl_id.uom_id.category_id.id)]
+                [('category_id', '=',
+                  self.product_tmpl_id.uom_id.category_id.id)]
         }
 
         product_lang = self.product_tmpl_id.with_context({
@@ -56,10 +44,60 @@ class PurchaseOrderLine(models.Model):
         fpos = self.order_id.fiscal_position_id
         company_id = self.env.user.company_id.id
         self.taxes_id = fpos.map_tax(
-            self.product_id.supplier_taxes_id.filtered(
+            self.product_tmpl_id.supplier_taxes_id.filtered(
                 lambda r: r.company_id.id == company_id)
         )
-        self._suggest_quantity()
-        self._onchange_quantity()
+        self._suggest_quantity_tmpl()
+        self._onchange_quantity_tmpl()
 
         return result
+
+    def _suggest_quantity_tmpl(self):
+        # this method is copied from the standard _suggest_quantity
+        if not self.product_tmpl_id:
+            return
+
+        seller_min_qty = self.product_tmpl_id.seller_ids \
+            .filtered(lambda r: r.name == self.order_id.partner_id) \
+            .sorted(key=lambda r: r.min_qty)
+        if seller_min_qty:
+            self.product_qty = seller_min_qty[0].min_qty or 1.0
+            self.product_uom = seller_min_qty[0].product_uom
+        else:
+            self.product_qty = 1.0
+
+    @api.onchange('product_qty', 'product_uom')
+    def _onchange_quantity_tmpl(self):
+        # this method is copied from the standard _onchange_quantity
+        if not self.product_tmpl_id:
+            return
+
+        #
+        seller = self.product_tmpl_id._select_seller(
+            partner_id=self.partner_id,
+            quantity=self.product_qty,
+            date=self.order_id.date_order and self.order_id.date_order[:10],
+            uom_id=self.product_uom)
+
+        if seller or not self.date_planned:
+            self.date_planned = self._get_date_planned(seller).strftime(
+                DEFAULT_SERVER_DATETIME_FORMAT)
+
+        if not seller:
+            return
+
+        price_unit = self.env['account.tax']._fix_tax_included_price(
+            seller.price, self.product_tmpl_id.supplier_taxes_id,
+            self.taxes_id) if seller else 0.0
+        if price_unit and seller and self.order_id.currency_id and \
+                seller.currency_id != self.order_id.currency_id:
+            price_unit = seller.currency_id.compute(price_unit,
+                                                    self.order_id.currency_id)
+
+        if seller and self.product_uom and \
+                seller.product_uom != self.product_uom:
+            price_unit = self.env['product.uom']._compute_price(
+                seller.product_uom.id, price_unit,
+                to_uom_id=self.product_uom.id)
+
+        self.price_unit = price_unit
