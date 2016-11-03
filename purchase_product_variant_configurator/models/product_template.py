3efaef50f9ca6fd83323f9d9f058125010635e6e
Zakaria Makrelouf <z.makrelouf@gmail.com>
2016-11-03 09:07:37 +0100

    [REF] remove unneeded import

--- purchase_product_variant_configurator/models/product_template.py
+++ purchase_product_variant_configurator/models/product_template.py
@@ -2,7 +2,7 @@
 # © 2016 ACSONE SA/NV
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 
-from openerp import api, fields, models
+from openerp import api, models
 from openerp.tools import DEFAULT_SERVER_DATE_FORMAT
 
 import time
