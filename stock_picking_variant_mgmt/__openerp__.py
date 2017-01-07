5ce1e3468c6b829152054d54402f66ebbc4b10b1
Pedro M. Baeza <pedro.baeza@gmail.com>
2017-01-07 21:02:54 +0100

    [ADD] stock_picking_variant_mgmt
    =================================================
    Handle easily multiple variants on Stock Pickings
    =================================================
    
    This module allows to add/modify of all the variants of a product in a direct
    screen without the need of handling them one by one.
    
    It also adds a convenient way of handling the transfer of the products in a
    2D matrix with all the values of the first attribute in columns, and the
    rest of the combinations in rows.
    
    Configuration
    =============
    
    * Configure your user to have any permission from "Inventory" group.
    * Create a product with 2 attributes and several values.
    
    Usage
    =====
    
    * Go to Inventory > Dashboard.
    * Create a new picking from one of the existing picking types.
    * Press "Add variants" button located in the upper right corner of the
      "Initial Demand" tab.
    * A new screen will appear allowing you to select the products that have
      variants.
    * Once you select the product, a 2D matrix will appear with the first
      attribute values as columns and the second one (if any) as rows.
    * If there are already order lines for the product variants, the current
      quantity will be pre-filled in the matrix.
    * Change the quantities for the variant you want and click on "Transfer to
      picking"
    * Move lines for the variants will be created/removed to comply with the
      input you have done.
    
    As extra feature for saving steps, there's also a button on each existing line
    that corresponds to a variant that opens the dialog directly with the product
    selected.
    
    You are also able to manage variants on 1 dimension in the transfer:
    
    * Go to the "Operations" page.
    * Press on "Manage Variants Transfer" button in the upper right corner of the
      tab.
    * Change the quantities to transfer.
    * Click on "Transfer to picking" button.
    
    Known issues / Roadmap
    ======================
    
    * The inline button for modifying quantities for an existing line won't
      work correctly until these 2 PRs are merged in Odoo:
    
      * https://github.com/odoo/odoo/pull/13557
      * https://github.com/odoo/odoo/pull/13635
    
      The patches are already integrated on OCB.
    
    * Make this work with product with more than 1 attribute.

---
+++ stock_picking_variant_mgmt/__openerp__.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+# Copyright 2017 Pedro M. Baeza <pedro.baeza@tecnativa.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    'name': 'Handle easily multiple variants on Stock Pickings',
+    'summary': 'Handle the addition/removal of multiple variants and the '
+               'quantities transferred in the Pickings.',
+    'version': '9.0.1.0.0',
+    'author': 'Tecnativa,'
+              'Odoo Community Association (OCA)',
+    'category': 'Inventory, Logistics, Warehousing',
+    'license': 'AGPL-3',
+    'website': 'https://www.tecnativa.com',
+    'depends': [
+        'stock',
+        'web_widget_x2many_2d_matrix',
+    ],
+    'demo': [],
+    'data': [
+        'wizard/stock_manage_variant_view.xml',
+        'wizard/stock_transfer_manage_variant_view.xml',
+        'views/stock_picking_view.xml',
+    ],
+    'installable': True,
+}
