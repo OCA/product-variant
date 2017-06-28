7ab392d9ce06f645c7cb5ad10b0454fa46663c18
Dario Lodeiros <dario@aloxa.eu>
2017-06-28 10:11:11 +0200

    [MIG] sale_variant_configurator_on_confirm: Migration to 9.0
    This comes from module sale_stock_product_variants in odoomrp-wip

---
+++ sale_variant_configurator_on_confirm/models/sale_order_line.py
@@ -0,0 +1,50 @@
+# -*- coding: utf-8 -*-
+# © 2016 Oihane Crucelaegui - AvanzOSC
+# © 2016 Pedro M. Baeza <pedro.baeza@tecnativa.com>
+# © 2016 ACSONE SA/NV
+# © 2016 Dario Lodeiros <dario@aloxa.eu>
+# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
+
+from openerp import api, fields, models
+from openerp.tools import DEFAULT_SERVER_DATETIME_FORMAT
+
+from datetime import datetime
+
+
+class SaleOrderLine(models.Model):
+    _inherit = ['sale.order.line', 'product.configurator']
+    _name = 'sale.order.line'
+
+    @api.onchange('product_tmpl_id')
+    def onchange_product_tmpl_id(self):
+        # this method is copied from the standard onchange_product_id
+        result = {}
+        if not self.product_tmpl_id:
+            return {}
+
+        self.date_planned = datetime.today().strftime(
+            DEFAULT_SERVER_DATETIME_FORMAT)
+        self.price_unit = self.product_qty = 0.0
+        self.product_uom = self.product_tmpl_id.uom_po_id or\
+            self.product_tmpl_id.uom_id
+        result['domain'] = {
+            'product_uom':
+                [('category_id', '=',
+                  self.product_tmpl_id.uom_id.category_id.id)]
+        }
+
+        product_lang = self.product_tmpl_id.with_context({
+            'lang': self.order_id.partner_id.lang,
+            'partner_id': self.order_id.partner_id.id,
+            'partner_id': self.order_id.partner_id.id,
+        })
+        if product_lang.description_sale:
+            self.name += '\n' + self.product_tmpl_id.description_sale
+
+        fpos = self.order_id.fiscal_position_id
+        company_id = self.env.user.company_id.id
+        self.taxes_id = fpos.map_tax(
+            self.product_tmpl_id.taxes_id.filtered(
+                lambda r: r.company_id.id == company_id)
+        )
+        return result
