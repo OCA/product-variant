9919b6a0326f6c3f38ba0893e01516af30790fb9
David Vidal <chienandalu@gmail.com>
2017-07-04 19:58:45 +0200

    [MIG] purchase_variant_configurator: Migration to 10.0

--- purchase_variant_configurator/hooks.py
+++ purchase_variant_configurator/hooks.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-# © 2014-2016 Oihane Crucelaegui - AvanzOSC
+# Copyright 2014-2016 Oihane Crucelaegui - AvanzOSC
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 
 
