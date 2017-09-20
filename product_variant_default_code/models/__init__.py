39b31092824ca73ea6b8c565c4340f473af1cad1
David Vidal <chienandalu@gmail.com>
2017-09-20 18:27:31 +0200

    [IMP] Merge product_code_builder functionalities

--- product_variant_default_code/models/__init__.py
+++ product_variant_default_code/models/__init__.py
@@ -1,3 +1,6 @@
-# -*- encoding: utf-8 -*-
+# -*- coding: utf-8 -*-
+# Copyright 2017 Tecnativa - David Vidal
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 from . import product
+from . import sale_config_settings
---
+++ product_variant_default_code/models/sale_config_settings.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+# Copyright 2017 Tecnativa - David Vidal
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from odoo import fields, models
+
+
+class SaleConfiguration(models.TransientModel):
+    _inherit = 'sale.config.settings'
+
+    group_product_default_code = fields.Selection([
+        (0, 'Automask'),
+        (1, 'Manual Mask')],
+        string='Product Default Code behaviour',
+        help='Set behaviour of codes',
+        implied_group='product_variant_default_code'
+                      '.group_product_default_code',
+    )
