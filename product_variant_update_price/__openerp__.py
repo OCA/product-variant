dc15fbd031768ece9aac017516155454b5ab8666
Stéphane Bidoul (ACSONE) <stephane.bidoul@acsone.eu>
2017-11-29 09:14:09 +0100

    [FIX] website URI (#78)

--- product_variant_update_price/__openerp__.py
+++ product_variant_update_price/__openerp__.py
@@ -8,8 +8,8 @@
     "summary": "Update price based on product template and attributes",
     "version": "8.0.1.0.0",
     "category": "Sales",
-    "website": "www.akretion.com",
-    "author": " Akretion",
+    "website": "https://github.com/OCA/product-variant",
+    "author": "Akretion",
     "license": "AGPL-3",
     "installable": True,
     "depends": [
