6a1a8b8ee5c70318550f2d3032ea71f0ad9cba83
PierrickBrun <pierrickjabrun@gmail.com>
2018-02-19 14:55:20 +0100

    [FIX] default reference mask creation

--- product_variant_default_code/__manifest__.py
+++ product_variant_default_code/__manifest__.py
@@ -7,7 +7,7 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 {
     'name': 'Product Variant Default Code',
-    'version': '10.0.1.0.0',
+    'version': '10.0.1.0.1',
     'author': 'AvancOSC,'
               'Shine IT,'
               'Tecnativa,'
