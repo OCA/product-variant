974fd6de0d2d9f07167a63f4b3c183f1cfe01793
ernesto <ernesto.tejeda87@gmail.com>
2018-10-07 17:45:59 -0400

    [MIG] purchase_variant_configurator: Migration to 11.0

--- purchase_variant_configurator/hooks.py
+++ purchase_variant_configurator/hooks.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 # Copyright 2014-2016 Oihane Crucelaegui - AvanzOSC
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 
--- purchase_variant_configurator/migrations/8.0.2.0.0/pre-migration.py
+++ purchase_variant_configurator/migrations/8.0.2.0.0/pre-migration.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 # © 2016 Oihane Crucelaegui - AvanzOSC
 # © 2016 Pedro M. Baeza <pedro.baeza@tecnativa.com>
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
