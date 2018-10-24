4f89222321d98d3835d9a4994fbd8dc69d1cc13f
angelmoya <amoyapardo@gmail.com>
2018-10-24 16:38:58 +0200

    [MIG] product_variant_configurator: Migration to 11.0

--- product_variant_configurator/models/product_attribute_price.py
+++ product_variant_configurator/models/product_attribute_price.py
@@ -1,6 +1,5 @@
-# -*- coding: utf-8 -*-
-# © 2015 Oihane Crucelaegui - AvanzOSC
-# © 2016 Pedro M. Baeza <pedro.baeza@tecnativa.com>
+# Copyright 2015 Oihane Crucelaegui - AvanzOSC
+# Copyright 2016 Pedro M. Baeza <pedro.baeza@tecnativa.com>
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3
 
 from odoo import fields, models
