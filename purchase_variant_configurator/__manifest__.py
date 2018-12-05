a457c51a683ef85e4f8f644c140ec0dad74ec93b
cubells <vicent@vcubells.net>
2018-12-05 10:28:58 +0100

    [FIX] sale_variant_configurator: attribute lines are not created

--- purchase_variant_configurator/__manifest__.py
+++ purchase_variant_configurator/__manifest__.py
@@ -6,7 +6,7 @@
 {
     "name": "Purchase - Product variants",
     "summary": "Product variants in purchase management",
-    'version': '11.0.1.0.0',
+    'version': '11.0.1.1.0',
     "license": "AGPL-3",
     "depends": [
         "purchase",
