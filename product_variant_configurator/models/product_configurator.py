51c10fb1f8bc89c6e58979d7c31e68e7db7642b3
Pedro M. Baeza <pedro.baeza@gmail.com>
2019-02-01 00:21:45 +0100

    [IMP] product_variant_configurator: Fill configurator vals in creation
    In some cases, like MTO/drop-shipping automatic purchase order line creation,
    `product_tmpl_id` and `product_attribute_ids` fields appear empty, because only
    `product_id` is supplied. We recreate them from the product_id overwriting
    generic `create` method and providing proper values.

--- product_variant_configurator/models/product_configurator.py
+++ product_variant_configurator/models/product_configurator.py
@@ -222,6 +222,26 @@ class ProductConfigurator(models.AbstractModel):
             return name
         return ("%s\n%s" if extended else "%s (%s)") % (name, description)
 
+    @api.model
+    def create(self, vals):
+        """Fill `product_tmpl_id` in case `product_id` is supplied but not the
+        other one.
+        """
+        if vals.get('product_id'):
+            product = self.env['product.product'].browse(vals['product_id'])
+            if not vals.get('product_tmpl_id'):
+                vals['product_tmpl_id'] = product.product_tmpl_id.id
+            if not vals.get('product_attribute_ids'):
+                vals['product_attribute_ids'] = []
+                gen_dict = {
+                    'owner_model': self._name,
+                    'product_tmpl_id': product.product_tmpl_id.id,
+                }
+                for att_val in product._get_product_attributes_values_dict():
+                    att_val.update(gen_dict)
+                    vals['product_attribute_ids'].append((0, 0, att_val))
+        return super().create(vals)
+
     @api.multi
     def unlink(self):
         """Mimic `ondelete="cascade"`."""
