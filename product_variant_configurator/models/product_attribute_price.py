a4bfeab02891da6ee783e7be79d370f06e7e8fdb
Pedro M. Baeza <pedro.baeza@tecnativa.com>
2019-06-20 14:29:30 +0200

    [FIX] product_variant_configurator: related as readonly for no side effects

--- product_variant_configurator/models/product_attribute_price.py
+++ product_variant_configurator/models/product_attribute_price.py
@@ -1,5 +1,5 @@
 # Copyright 2015 Oihane Crucelaegui - AvanzOSC
-# Copyright 2016 Pedro M. Baeza <pedro.baeza@tecnativa.com>
+# Copyright 2016-2019 Pedro M. Baeza <pedro.baeza@tecnativa.com>
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3
 
 from odoo import fields, models
@@ -9,4 +9,7 @@ class ProductAttributePrice(models.Model):
     _inherit = 'product.attribute.price'
 
     attribute_id = fields.Many2one(
-        comodel_name='product.attribute', related='value_id.attribute_id')
+        comodel_name='product.attribute',
+        related='value_id.attribute_id',
+        readonly=True,
+    )
