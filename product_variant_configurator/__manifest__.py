d336761f5b851ced0911f5ac7d4f71179e441ee6
HviorForgeFlow <hector.villarreal@forgeflow.com>
2020-09-14 16:58:01 +0200

    [IMP] product_variant_configurator: black, isort, prettier

--- product_variant_configurator/__manifest__.py
+++ product_variant_configurator/__manifest__.py
@@ -5,26 +5,24 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
-    'name': 'Product Variant Configurator',
-    'summary': "Provides an abstract model for product variant configuration.",
-    'version': '12.0.1.0.1',
-    'category': 'Product Variant',
-    'license': 'AGPL-3',
-    'author': 'AvanzOSC, '
-              'Tecnativa, '
-              'ACSONE SA/NV, '
-              'Odoo Community Association (OCA)',
-    'website': 'https://github.com/OCA/product-variant',
-    'depends': [
-        "product",
-    ],
-    'data': [
-        'security/product_configurator_security.xml',
-        'security/ir.model.access.csv',
-        'views/product_configurator_attribute.xml',
-        'views/inherited_product_template_views.xml',
-        'views/inherited_product_product_views.xml',
-        'views/inherited_product_category_views.xml',
-        'views/inherited_product_attribute_views.xml',
+    "name": "Product Variant Configurator",
+    "summary": "Provides an abstract model for product variant configuration.",
+    "version": "12.0.1.0.1",
+    "category": "Product Variant",
+    "license": "AGPL-3",
+    "author": "AvanzOSC, "
+    "Tecnativa, "
+    "ACSONE SA/NV, "
+    "Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/product-variant",
+    "depends": ["product",],
+    "data": [
+        "security/product_configurator_security.xml",
+        "security/ir.model.access.csv",
+        "views/product_configurator_attribute.xml",
+        "views/inherited_product_template_views.xml",
+        "views/inherited_product_product_views.xml",
+        "views/inherited_product_category_views.xml",
+        "views/inherited_product_attribute_views.xml",
     ],
 }
--- product_variant_configurator/models/product_attribute_value.py
+++ product_variant_configurator/models/product_attribute_value.py
@@ -5,7 +5,7 @@ from odoo import api, models
 
 
 class ProductAttributeValue(models.Model):
-    _inherit = 'product.attribute.value'
+    _inherit = "product.attribute.value"
 
     @api.model
     def create(self, vals):
@@ -14,10 +14,12 @@ class ProductAttributeValue(models.Model):
         This happens when quick-creating values from the product configurator.
         """
         attr_value = super(ProductAttributeValue, self).create(vals)
-        if 'template_for_attribute_value' in self.env.context:
-            template = self.env['product.template'].browse(
-                self.env.context['template_for_attribute_value'])
+        if "template_for_attribute_value" in self.env.context:
+            template = self.env["product.template"].browse(
+                self.env.context["template_for_attribute_value"]
+            )
             line = template.attribute_line_ids.filtered(
-                lambda x: x.attribute_id == attr_value.attribute_id)
+                lambda x: x.attribute_id == attr_value.attribute_id
+            )
             line.value_ids = [(4, attr_value.id)]
         return attr_value
