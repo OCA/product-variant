db43690c8a8485b7142c7f8e469a98d5b0990d9a
Radovan Skolnik <radovan@skolnik.info>
2020-09-29 10:11:57 +0200

    [IMP] product_variant_sale_price: black, isort, prettier

--- product_variant_sale_price/__manifest__.py
+++ product_variant_sale_price/__manifest__.py
@@ -3,21 +3,15 @@
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 
 {
-    'name': 'Product Variant Sale Price',
-    'summary': 'Allows to write fixed prices in product variants',
-    'version': '12.0.1.0.0',
-    'category': 'Product Management',
-    'website': 'https://odoo-community.org/',
-    'author': 'Tecnativa, '
-              'Odoo Community Association (OCA)',
-    'license': 'AGPL-3',
-    'installable': True,
-    'depends': [
-        'account',
-        'sale',
-    ],
-    'data': [
-        'views/product_views.xml',
-    ],
-    'post_init_hook': 'set_sale_price_on_variant',
+    "name": "Product Variant Sale Price",
+    "summary": "Allows to write fixed prices in product variants",
+    "version": "12.0.1.0.0",
+    "category": "Product Management",
+    "website": "https://odoo-community.org/",
+    "author": "Tecnativa, " "Odoo Community Association (OCA)",
+    "license": "AGPL-3",
+    "installable": True,
+    "depends": ["account", "sale",],
+    "data": ["views/product_views.xml",],
+    "post_init_hook": "set_sale_price_on_variant",
 }
