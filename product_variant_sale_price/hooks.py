fc22195c53b16020432c77de4deff241a93f56f3
Radovan Skolnik <radovan@skolnik.info>
2020-09-29 15:37:06 +0200

    [MIG] product_variant_sale_price: Migration to 13.0

--- product_variant_sale_price/hooks.py
+++ product_variant_sale_price/hooks.py
@@ -8,11 +8,10 @@ def set_sale_price_on_variant(cr, registry, template_id=None):
         UPDATE product_product pp
         SET fix_price = pt.list_price + (
             SELECT COALESCE(SUM(ptav.price_extra), 0)
-            FROM product_attribute_value_product_product_rel pav_pp_rel
+            FROM product_variant_combination pvc
             LEFT JOIN product_template_attribute_value ptav ON
-                ptav.product_attribute_value_id =
-                pav_pp_rel.product_attribute_value_id
-            WHERE pav_pp_rel.product_product_id = pp.id
+                ptav.id = pvc.product_template_attribute_value_id
+            WHERE pvc.product_product_id = pp.id
             AND ptav.product_tmpl_id = pt.id
         )
         FROM product_template pt
