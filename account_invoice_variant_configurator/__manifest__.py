6310568a37d5dc1b6f223663e69b8583da59f0f5
Thomas Binsfeld <thomas.binsfeld@acsone.eu>
2020-10-23 14:13:21 +0200

    [MIG] account_invoice_variant_configurator from 9.0 to 13.0

--- account_invoice_variant_configurator/__manifest__.py
+++ account_invoice_variant_configurator/__manifest__.py
@@ -5,11 +5,10 @@
     "name": "Invoice Product Variant Configurator",
     "summary": """
         Product variant configurator on invoice lines""",
-    "version": "9.0.1.0.0",
+    "version": "13.0.1.0.0",
     "license": "AGPL-3",
     "author": "ACSONE SA/NV,Odoo Community Association (OCA)",
     "website": "https://acsone.eu/",
     "depends": ["account", "product_variant_configurator"],
-    "data": ["views/account_invoice.xml", "views/account_invoice_line.xml"],
-    "demo": [],
+    "data": ["views/account_move.xml"],
 }
