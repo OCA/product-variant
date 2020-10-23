6cd165548542faebb56e23c4666596e95bd7582e
Thomas Binsfeld <thomas.binsfeld@acsone.eu>
2020-10-23 14:12:43 +0200

    [MIG] account_invoice_variant_configurator: run pre-commit

--- account_invoice_variant_configurator/models/account_invoice_line.py
+++ account_invoice_variant_configurator/models/account_invoice_line.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 # Copyright 2016 ACSONE SA/NV
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
@@ -7,5 +6,5 @@ from openerp import models
 
 class AccountInvoiceLine(models.Model):
 
-    _inherit = ['account.invoice.line', 'product.configurator']
-    _name = 'account.invoice.line'
+    _inherit = ["account.invoice.line", "product.configurator"]
+    _name = "account.invoice.line"
