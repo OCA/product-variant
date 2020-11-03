4ad4b71e804c93daf8f66411d282105ecedfeebb
João Marques <joao.marques@tecnativa.com>
2020-11-03 09:45:08 +0000

    [MIG] product_variant_default_code: Migration to 13.0
    TT26450

--- product_variant_default_code/__manifest__.py
+++ product_variant_default_code/__manifest__.py
@@ -8,7 +8,7 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 {
     "name": "Product Variant Default Code",
-    "version": "13.0.1.0.0",
+    "version": "13.0.1.0.1",
     "author": "AvancOSC," "Shine IT," "Tecnativa," "Odoo Community Association (OCA)",
     "website": "https://odoo-community.org/",
     "license": "AGPL-3",
@@ -17,7 +17,6 @@
     "data": [
         "security/product_security.xml",
         "data/ir_config_parameter.xml",
-        "views/product_attribute_value_view.xml",
         "views/product_attribute_view.xml",
         "views/product_view.xml",
         "views/config_settings_view.xml",
