ac41a42e2d0208a4ee6802e5d6c4bd75fe3f1f1a
João Marques <joao.marques@tecnativa.com>
2020-11-10 11:12:39 +0000

    [MIG] product_variant_default_code: Migration to 14.0
    TT26452

--- product_variant_default_code/__manifest__.py
+++ product_variant_default_code/__manifest__.py
@@ -8,7 +8,7 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 {
     "name": "Product Variant Default Code",
-    "version": "13.0.1.0.1",
+    "version": "14.0.1.0.0",
     "author": "AvancOSC," "Shine IT," "Tecnativa," "Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/product-variant",
     "license": "AGPL-3",
