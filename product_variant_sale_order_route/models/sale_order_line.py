3f070e50c90bd7a161e4967d8192401e3491e82d
Karl Southern <karl@theangryangel.co.uk>
2020-11-16 10:57:46 +0000

    Ensure product_variant_sale_order_route works correctly in 12.0

--- product_variant_sale_order_route/models/sale_order_line.py
+++ product_variant_sale_order_route/models/sale_order_line.py
@@ -2,16 +2,20 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 
-from odoo import models
+from odoo import models, api
 
 
 class SaleOrderLine(models.Model):
     _inherit = 'sale.order.line'
 
+    @api.multi
+    @api.onchange('product_id')
     def product_id_change(self):
-        res = super().product_id_change()
-        if self.product_id:
-            self.update(
-                {'route_id': self.product_id.sale_order_line_route_id.id}
-            )
+        res = super(SaleOrderLine, self).product_id_change()
+
+        for record in self:
+            if record.product_id:
+                record.update(
+                    {'route_id': self.product_id.sale_order_line_route_id.id}
+                )
         return res
