3b1ca5695c5b2637ee803bc81d4ccc9287359c77
Héctor Villarreal Ortega <hector.villarreal@forgeflow.com>
2021-01-20 09:05:22 +0100

    [13.0][FIXUP] Show all attributes: fixup for FAME

--- product_variant_configurator/models/product_template_attribute_value.py
+++ product_variant_configurator/models/product_template_attribute_value.py
@@ -9,4 +9,4 @@ class ProductTemplateAttributeValue(models.Model):
     def _get_combination_name(self):
         """Overwritten method to avoid:
         Exclude values from single value lines or from no_variant attributes."""
-        return ", ".join([ptav.name for ptav in self._filter_single_value_lines()])
+        return ", ".join([ptav.name for ptav in self])
