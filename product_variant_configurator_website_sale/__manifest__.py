2b0bbea789ae0f94209b5f623e838a12daccc823
Rad0van <radovan@skolnik.info>
2021-06-24 09:42:23 +0200

    [13.0][NEW]product_variant_configurator_website_sale

---
+++ product_variant_configurator_website_sale/__manifest__.py
@@ -0,0 +1,13 @@
+# Copyright 2021 - Radovan Skolnik
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+{
+    "name": "Product Variant Configurator glue to Website Sale",
+    "version": "13.0.1.0.0",
+    "author": "Radovan Skolnik," "Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/product-variant",
+    "license": "AGPL-3",
+    "category": "Product",
+    "depends": ["product_variant_configurator", "website_sale"],
+    "installable": True,
+    "auto_install": True,
+}
---
+++ product_variant_configurator_website_sale/readme/CONFIGURE.rst
@@ -0,0 +1,3 @@
+(after installing both `website_sale` and `product_variant_configurator`)
+
+No configuration is needed, the module should be installed automatically.
