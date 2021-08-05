1c59f38cbd34943e4c79185b293b8974ffc7fec7
Simone Orsi <simahawk@gmail.com>
2021-08-05 17:05:27 +0200

    product_variant_change_attribute_value: reorganize code

--- product_variant_change_attribute_value/wizards/product_variant_attribute_value_wizard.py
+++ product_variant_change_attribute_value/wizards/product_variant_attribute_value_wizard.py
@@ -66,9 +66,6 @@ class VariantAttributeValueWizard(models.TransientModel):
 
     def _action_apply(self, product):
         """Update a variant with all the actions set by the user in the wizard."""
-        TplAttrLine = self.env["product.template.attribute.line"]
-        TplAttrValue = self.env["product.template.attribute.value"]
-        template = product.product_tmpl_id
         pav_ids = product.product_template_attribute_value_ids.mapped(
             "product_attribute_value_id"
         )
@@ -79,66 +76,78 @@ class VariantAttributeValueWizard(models.TransientModel):
             pav = value_action.product_attribute_value_id
             if pav not in pav_ids:
                 continue
-            pav_replacement = value_action.replaced_by_id
-            if action == "replace" and not pav_replacement:
-                continue
-            elif action == "delete":
-                pass
-            elif action == "replace" and pav_replacement:
-                # Find corresponding attribute line on template or create it
-                attr = pav_replacement.attribute_id
-                tpl_attr_line = template.attribute_line_ids.filtered(
-                    lambda l: l.attribute_id == attr
-                )
-                if not tpl_attr_line:
-                    tpl_attr_line = TplAttrLine.create(
-                        {
-                            "product_tmpl_id": template.id,
-                            "attribute_id": attr.id,
-                            "value_ids": [(6, False, [pav_replacement.id])],
-                        }
-                    )
-                # Ensure the value exists in this attribute line.
-                # The context key 'update_product_template_attribute_values' avoids
-                # to create/unlink variants when values are updated on the template
-                # attribute line.
-                tpl_attr_line.with_context(
-                    update_product_template_attribute_values=False
-                ).write({"value_ids": [(4, pav_replacement.id)]})
-                # Get (or create if needed) the 'product.template.attribute.value'
-                tpl_attr_value = TplAttrValue.search(
-                    [
-                        ("attribute_line_id", "=", tpl_attr_line.id),
-                        ("product_attribute_value_id", "=", pav_replacement.id),
-                    ]
-                )
-                if not tpl_attr_value:
-                    tpl_attr_value = TplAttrValue.create(
-                        {
-                            "attribute_line_id": tpl_attr_line.id,
-                            "product_attribute_value_id": pav_replacement.id,
-                        }
-                    )
-            # Update the values set on the product variant
             ptav_ids = product.product_template_attribute_value_ids.filtered(
                 lambda r: r.product_attribute_value_id != pav
             )
-            if action == "replace":
+            pav_replacement = value_action.replaced_by_id
+            if action == "delete":
+                pass
+            elif action == "replace" and not pav_replacement:
+                continue
+            elif action == "replace" and pav_replacement:
+                tpl_attr_value = self._handle_replace(product, pav_replacement)
                 ptav_ids |= tpl_attr_value
+
+            # Update the values set on the product variant
             product.product_template_attribute_value_ids = ptav_ids
             # Remove the changed value from the template attribute line if needed
             if not self._is_attribute_value_being_used(product, pav):
-                tpl_attr_line = template.attribute_line_ids.filtered(
-                    lambda l: l.attribute_id == pav.attribute_id
-                )
-                tpl_attr_line.with_context(
-                    update_product_template_attribute_values=False
-                ).write({"value_ids": [(3, pav.id)]})
-                tpl_attr_value = TplAttrValue.search(
-                    [
-                        ("attribute_line_id", "=", tpl_attr_line.id),
-                        ("product_attribute_value_id", "=", pav.id),
-                    ]
-                )
-                if tpl_attr_value:
-                    tpl_attr_value.unlink()
+                self._cleanup_attribute_value(product, pav)
+
+    def _handle_replace(self, product, pav_replacement):
+        TplAttrLine = self.env["product.template.attribute.line"]
+        TplAttrValue = self.env["product.template.attribute.value"]
+        template = product.product_tmpl_id
+        # Find corresponding attribute line on template or create it
+        attr = pav_replacement.attribute_id
+        tpl_attr_line = template.attribute_line_ids.filtered(
+            lambda l: l.attribute_id == attr
+        )
+        if not tpl_attr_line:
+            tpl_attr_line = TplAttrLine.create(
+                {
+                    "product_tmpl_id": template.id,
+                    "attribute_id": attr.id,
+                    "value_ids": [(6, False, [pav_replacement.id])],
+                }
+            )
+        # Ensure the value exists in this attribute line.
+        # The context key 'update_product_template_attribute_values' avoids
+        # to create/unlink variants when values are updated on the template
+        # attribute line.
+        tpl_attr_line.with_context(
+            update_product_template_attribute_values=False
+        ).write({"value_ids": [(4, pav_replacement.id)]})
+        # Get (or create if needed) the 'product.template.attribute.value'
+        tpl_attr_value = TplAttrValue.search(
+            [
+                ("attribute_line_id", "=", tpl_attr_line.id),
+                ("product_attribute_value_id", "=", pav_replacement.id),
+            ]
+        )
+        if not tpl_attr_value:
+            tpl_attr_value = TplAttrValue.create(
+                {
+                    "attribute_line_id": tpl_attr_line.id,
+                    "product_attribute_value_id": pav_replacement.id,
+                }
+            )
+        return tpl_attr_value
+
+    def _cleanup_attribute_value(self, product, pav):
+        TplAttrValue = self.env["product.template.attribute.value"]
+        template = product.product_tmpl_id
+        tpl_attr_line = template.attribute_line_ids.filtered(
+            lambda l: l.attribute_id == pav.attribute_id
+        )
+        tpl_attr_line.with_context(
+            update_product_template_attribute_values=False
+        ).write({"value_ids": [(3, pav.id)]})
+        tpl_attr_value = TplAttrValue.search(
+            [
+                ("attribute_line_id", "=", tpl_attr_line.id),
+                ("product_attribute_value_id", "=", pav.id),
+            ]
+        )
+        if tpl_attr_value:
+            tpl_attr_value.unlink()
