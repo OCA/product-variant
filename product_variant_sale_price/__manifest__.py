e291e3c2f68ad44f8d0c9408509b40b046444d30
Maksym Yankin <yankinmk@gmail.com>
2022-01-31 16:32:27 +0200

    [MIG] product_variant_sale_price: Migration to 15.0

--- product_variant_sale_price/__manifest__.py
+++ product_variant_sale_price/__manifest__.py
@@ -5,7 +5,7 @@
 {
     "name": "Product Variant Sale Price",
     "summary": "Allows to write fixed prices in product variants",
-    "version": "14.0.1.0.0",
+    "version": "15.0.1.0.0",
     "category": "Product Management",
     "website": "https://github.com/OCA/product-variant",
     "author": "Tecnativa, " "Odoo Community Association (OCA)",
