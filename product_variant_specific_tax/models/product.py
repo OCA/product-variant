2275f7126191293cb5ce63a5ba8a50936a3b3d94
ellbristow <elliott@glo.systems>
2022-02-08 11:23:26 +0100

    Initial copy 11.0 to 14.0

--- product_variant_specific_tax/models/product.py
+++ product_variant_specific_tax/models/product.py
@@ -2,14 +2,14 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 
-from odoo import models, fields
+from odoo import fields, models
 
 
 class Product(models.Model):
-    _inherit = 'product.product'
+    _inherit = "product.product"
 
     additional_tax_ids = fields.Many2many(
-        comodel_name='account.tax',
+        comodel_name="account.tax",
         string="Additional Taxes",
-        domain=[('type_tax_use', '=', 'sale')],
+        domain=[("type_tax_use", "=", "sale")],
     )
