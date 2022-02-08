7bef08d839c4e179b2f3aef92da2767d1cdabfb0
ellbristow <elliott@glo.systems>
2022-02-08 11:24:19 +0100

    Update manifest for 14.0

--- product_variant_specific_tax/__manifest__.py
+++ product_variant_specific_tax/__manifest__.py
@@ -2,9 +2,10 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 {
     "name": "Product Variant Specific Tax",
-    "version": "11.0.1.0.0",
+    "version": "14.0.1.0.0",
     "category": "Product",
     "author": "Camptocamp SA, " "Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/product-variant",
     "license": "AGPL-3",
     "depends": [
         "account",
@@ -13,9 +14,5 @@
     ],
     "data": [
         "views/product_view.xml",
-    ],
-    "installable": True,
-    "images": [
-        "static/description/icon.png",
     ],
 }
