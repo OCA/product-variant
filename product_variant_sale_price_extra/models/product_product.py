813125d062dd82b335b9aae09b4c5dcd99c9d5dc
Ilyas <irazor147@gmail.com>
2022-05-02 13:20:14 +0700

    [14.0][REF] product_variant_sale_price_extra

--- product_variant_sale_price_extra/models/product_product.py
+++ product_variant_sale_price_extra/models/product_product.py
@@ -4,7 +4,10 @@ from odoo import fields, models
 class ProductProduct(models.Model):
     _inherit = "product.product"
 
-    variant_sale_price = fields.Float('Variant Sale Price (extra prices included)', compute='_compute_variant_sale_price')
+    variant_sale_price = fields.Float(
+        "Variant Sale Price (extra prices included)",
+        compute="_compute_variant_sale_price",
+    )
 
     def _compute_variant_sale_price(self):
         for product in self:
