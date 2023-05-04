4b43b083f24a3c2b4b890836f1f497904dc5ec62
RabbitJon-S73 <roger@studio73.es>
2023-05-04 08:34:36 +0200

    [FIX] product_variant_default_code: code_prefix in reference_mask

--- product_variant_default_code/models/product.py
+++ product_variant_default_code/models/product.py
@@ -123,7 +123,11 @@ class ProductTemplate(models.Model):
                 rec.code_prefix = rec.default_code
             if automask or not rec.reference_mask:
                 rec.reference_mask = rec._get_default_mask()
-            elif not automask and rec.code_prefix:
+            elif (
+                not automask
+                and rec.code_prefix
+                and rec.code_prefix not in rec.reference_mask
+            ):
                 rec.reference_mask = rec.code_prefix + rec.reference_mask
 
     def _inverse_reference_mask(self):
