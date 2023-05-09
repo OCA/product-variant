d0ed7e549de884a12517359848d1f2d10a9c295e
dsolanki <dhara.solanki@initos.com>
2023-05-09 17:27:16 +0530

    [MIG] sale_order_line_variant_description: Migration to 16.0

--- sale_order_line_variant_description/models/sale_order_line.py
+++ sale_order_line_variant_description/models/sale_order_line.py
@@ -8,8 +8,8 @@ class SaleOrderLine(models.Model):
     _inherit = "sale.order.line"
 
     @api.onchange("product_id")
-    def product_id_change(self):
-        res = super(SaleOrderLine, self).product_id_change()
+    def _onchange_product_id_warning(self):
+        res = super(SaleOrderLine, self)._onchange_product_id_warning()
         if self.product_id:
             product = self.product_id.with_context(lang=self.order_id.partner_id.lang)
             if product.variant_description_sale:
