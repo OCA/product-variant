3a32dc2685de01ca768d58c359e5351de752d39e
JordiMForgeFlow <jordi.masvidal@forgeflow.com>
2023-12-19 11:24:22 +0100

    [IMP] sale_order_line_variant_description: hide template field in product variant view
    [UPD] Update sale_order_line_variant_description.pot
    
    [BOT] post-merge updates
    
    Update translation files
    
    Updated by "Update PO files to match POT (msgmerge)" hook in Weblate.
    
    Translation: product-variant-16.0/product-variant-16.0-sale_order_line_variant_description
    Translate-URL: https://translation.odoo-community.org/projects/product-variant-16-0/product-variant-16-0-sale_order_line_variant_description/
    
    Translated using Weblate (Italian)
    
    Currently translated at 100.0% (7 of 7 strings)
    
    Translation: product-variant-16.0/product-variant-16.0-sale_order_line_variant_description
    Translate-URL: https://translation.odoo-community.org/projects/product-variant-16-0/product-variant-16-0-sale_order_line_variant_description/it/
    
    Translated using Weblate (Portuguese (Brazil))
    
    Currently translated at 100.0% (7 of 7 strings)
    
    Translation: product-variant-16.0/product-variant-16.0-sale_order_line_variant_description
    Translate-URL: https://translation.odoo-community.org/projects/product-variant-16-0/product-variant-16-0-sale_order_line_variant_description/pt_BR/

--- sale_order_line_variant_description/__manifest__.py
+++ sale_order_line_variant_description/__manifest__.py
@@ -3,7 +3,7 @@
 
 {
     "name": "Sale order line variant description",
-    "version": "16.0.1.0.1",
+    "version": "16.0.1.1.0",
     "category": "Sales Management",
     "author": "Agile Business Group, Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/product-variant",
