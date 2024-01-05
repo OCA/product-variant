a027cee5f4f712f61bd336e849ed29fe30956408
Carolina Fernandez <carolina.fernandez@tecnativa.com>
2024-01-05 17:17:48 -0300

    [MIG] product_variant_attribute_tax: Migration to 16.0

--- product_variant_attribute_tax/__manifest__.py
+++ product_variant_attribute_tax/__manifest__.py
@@ -1,4 +1,5 @@
 # Copyright 2016-2017 Tecnativa - Pedro M. Baeza
+# Copyright 2024 Tecnativa - Carolina Fernandez
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 
 {
@@ -7,7 +8,7 @@
     "author": "Tecnativa, Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/product-variant",
     "category": "Product Management",
-    "version": "13.0.1.0.0",
+    "version": "16.0.1.0.0",
     "license": "AGPL-3",
     "depends": ["product", "account"],
     "data": ["views/product_attribute_views.xml"],
