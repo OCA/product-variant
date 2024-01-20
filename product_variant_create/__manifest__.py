e4a1936d204e349296fe7de42b2ebbbe05b5d168
rosenvladimirov <rosen.vladimirov@mecproduction.eu>
2024-01-20 11:25:18 +0200

    Fix errors in variant create

--- product_variant_create/__manifest__.py
+++ product_variant_create/__manifest__.py
@@ -2,21 +2,20 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 
 {
-    'name': 'Product Variant Create',
-    'summary': """
+    "name": "Product Variant Create",
+    "summary": """
         Wizard for create variant""",
-    'version': '16.0.1.0.0',
-    'license': 'AGPL-3',
-    'author': 'Rosen Vladimirov, BioPrint Ltd.,Odoo Community Association (OCA)',
-    'website': 'https://github.com/OCA/product-variant',
-    'depends': [
-        'product',
-        'stock',
+    "version": "16.0.1.0.0",
+    "license": "AGPL-3",
+    "author": "Rosen Vladimirov, BioPrint Ltd.,Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/product-variant",
+    "depends": [
+        "product",
+        "stock",
     ],
-    'data': [
-        'security/ir.model.access.csv',
-        'wizards/product_variant_create.py.xml',
+    "data": [
+        "security/ir.model.access.csv",
+        "wizards/product_variant_create.py.xml",
     ],
-    'demo': [
-    ],
+    "demo": [],
 }
