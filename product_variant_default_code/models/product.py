a3db18447fd51e22ac58ee1a80c3a824c2948a5a
andrea <ast@360erp.com>
2024-04-08 15:07:17 +0200

    [MIG] product_variant_default_code: pre-commit

--- product_variant_default_code/models/product.py
+++ product_variant_default_code/models/product.py
@@ -146,7 +146,7 @@ class ProductTemplate(models.Model):
         main_lang = self._guess_main_lang()
         for line in self.attribute_line_ids:
             attribute_names.append(
-                "[{}]".format(line.attribute_id.with_context(lang=main_lang).name)
+                f"[{line.attribute_id.with_context(lang=main_lang).name}]"
             )
         default_mask = (self.code_prefix or "") + default_reference_separator.join(
             attribute_names
