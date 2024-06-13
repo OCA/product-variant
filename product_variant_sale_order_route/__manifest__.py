610f199ba4e81e495a9f3a8bcbdaacbc8fce0093
Vincent Van Rossem <vincent.vanrossem@camptocamp.com>
2024-06-13 17:19:23 +0200

    Replace 'Camptocamp SA' with 'Camptocamp'

--- product_variant_sale_order_route/__manifest__.py
+++ product_variant_sale_order_route/__manifest__.py
@@ -4,7 +4,7 @@
     'name': 'Product Variant Sale Order Route',
     'version': '11.0.1.0.0',
     'category': 'Product',
-    'author': 'Camptocamp SA, '
+    'author': 'Camptocamp, '
               'Odoo Community Association (OCA)',
     'license': 'AGPL-3',
     'depends': [
