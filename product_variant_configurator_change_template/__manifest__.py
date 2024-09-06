6a587d2ed77822845d3fb900329b9695c20067b3
Christopher Ormaza <chris.ormaza@forgeflow.com>
2024-09-06 16:02:12 -0500

    [16.0][ADD] product_variant_configurator_change_template

---
+++ product_variant_configurator_change_template/__manifest__.py
@@ -0,0 +1,23 @@
+# Copyright 2025 ForgeFlow
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Product Variant Change Template",
+    "version": "16.0.1.0.0",
+    "summary": "Tool to allow change template of a existing product variant",
+    "category": "Tools",
+    "author": "ForgeFlow S.L, Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/product-variant",
+    "license": "AGPL-3",
+    "depends": ["product_variant_configurator", "product_variant_sale_price"],
+    "data": [
+        "security/ir.model.access.csv",
+        "wizard/wizard_product_variant_change_template_view.xml",
+    ],
+    "demo": [],
+    "installable": True,
+    "auto_install": False,
+    "external_dependencies": {
+        "python": [],
+    },
+}
